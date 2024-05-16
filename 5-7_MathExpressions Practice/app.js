f8e3d8f410e2d21d3c9cd8517eb17b3b8cd89011
FaizRaza2663 <faiz11122663@gmail.com>
2024-05-16 07:12:43 +0500

    Practice chp 5-7 Ques 1-5

---
+++ 5-7_MathExpressions Practice/app.js
@@ -0,0 +1,89 @@
+// 1. Write a program that take two numbers & add them in a new variable. Show the result in your browser.
+// var sum = 13 + 19
+// alert("Sum of 13 and 19 is " + sum)
+
+// 2. Repeat task1 for subtraction, multiplication, division & modulus
+
+// Subtraction
+// var num1 = +prompt("Enter your First number")
+// var num2 = +prompt("Enter your Second number")
+// var sub = num1 - num2
+// alert(sub)
+
+// Multiplication
+// var num1 = +prompt("Enter your First number")
+// var num2 = +prompt("Enter your Second number")
+// var mult = num1 * num2
+// alert(mult)
+
+// Division
+// var num1 = +prompt("Enter your First number")
+// var num2 = +prompt("Enter your Second number")
+// var div = num1 / num2
+// alert(div)
+
+// Modulus
+// var num1 = +prompt("Enter your First number")
+// var num2 = +prompt("Enter your Second number")
+// var mod = num1 % num2
+// alert(mod)
+
+// 3. Do the following using JS Mathematic Expressions
+// a. Declare a variable.
+// b. Show the value of variable in your browser like “Value after variable declaration is: ??”.
+// c. Initialize the variable with some number.
+// d. Show the value of variable in your browser like “Initial value: 3”.
+// e. Increment the variable.
+// f. Show the value of variable in your browser like “Value after increment is: 4”.
+// g. Add 7 to the variable.
+// h. Show the value of variable in your browser like “Value after addition is: 11”.
+// i. Decrement the variable.
+// j. Show the value of variable in your browser like “Value after decrement is: 10”.
+// k. Show the remainder after dividing the variable’s value by 3. Output : “The remainder is : 1”
+
+// var num;
+// console.log("Value after variable declaration is: " + num);
+// num = 3
+// console.log("Initial value: " + num)
+// num++
+// console.log("Value after incrament is: " + num)
+// num = num + 7
+// console.log("Value after addition is: " + num)
+// num--
+// console.log("Value after decrement is: " + num)
+// num = num % 3
+// console.log("The reminder is: " + num)
+
+// 4. Cost of one movie ticket is 600 PKR. Write a script to store ticket price in a variable & calculate the cost of buying 5 tickets to a movie.
+
+// alert("Cost of one ticket is 600 PKR");
+// var numOfUserTicket = +prompt("How many tickets do you buy?")
+// var priceOfTickets = 600
+// var totalCost = 600 * numOfUserTicket
+// alert("Total cost to buy " + numOfUserTicket + " tickets to a movie is " + totalCost + " PKR")
+
+// 5. Write a script to display multiplication table of any number in your browser.
+
+// var numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+// for(var i = 0; i < numbers.length; i++){
+//     var table = 4 * numbers[i]
+//     console.log("4 x " + numbers[i] + " = " + table)
+// }
+
+
+// 6. The Temperature Converter: It’s hot out! Let’s make a converter based on the steps here.
+// a. Store a Celsius temperature into a variable.
+// b. Convert it to Fahrenheit & output “NNoC is NNoF”.
+// c. Now store a Fahrenheit temperature into a variable.
+// d. Convert it to Celsius & output “NNoF is NNoC”.
+
+
+// Converting Celsius to Fahreheit
+// var celsiusTempuratue = +prompt("Please enter tempurature in Celsius")
+// var convertCelsiusToFahrenheit = (celsiusTempuratue * 9 / 5) + 32
+// alert("Your Fahrenheit tempurature is " + convertCelsiusToFahrenheit + " F" )
+
+// Converting Fahrenheit to Celsius
+// var fahrenheitTempurature = +prompt("Please enter tempurature in Fahrenheit")
+// var convertFahrenheitToCelsius = (fahrenheitTempurature - 32) * 5 / 9
+// alert("Your Celsius tempurature is " + convertFahrenheitToCelsius + " C")
